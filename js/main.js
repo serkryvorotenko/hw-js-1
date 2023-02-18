f5dc8ce9eeb5361c15f5a44869a52acb4b26ecc7
Serhii Kryvorotenko <serhii@MacBook-Pro-Serhii.local>
2023-02-18 16:03:40 +0100

    finish

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const userName = prompt('Как тебя зовут?');
+const userYear = prompt('Какого ты года рождения?');
+const userCountry = prompt('С какой ты страны?');
+const userAge = 2023 - userYear;
+const message = 'Привет, ' + userName + '! Твой возраст: ' + userAge + '. Твоя страна: ' + userCountry + '.';
+alert(message);
